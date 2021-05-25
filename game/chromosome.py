d1b470bfcb536d2511fc3b6dc6cdae80349289c3
baharKaviani <bhr.kaviani@gmail.com>
2021-05-25 15:11:44 +0430

    add: crossOver and mutation and check_repeat

--- game/chromosome.py
+++ game/chromosome.py
@@ -6,6 +6,7 @@ class Population:
 
         self.level = level
         self.population_score = dict()
+        self.selectList = None
 
 
     # TODO: make 200 chromosome
@@ -16,12 +17,18 @@ class Population:
             p = ""
             for j in range(len(self.level)):
                 t = random.randint(0, 2)
+                if j > 0:
+                    while t == 1 and p[j - 1] == '1':
+                        t = random.randint(0, 2)
+
+                    while t == 2 and p[j - 1] == '2':
+                        t = random.randint(0, 2)
                 p = p + str(t)
             self.population_score[p] = 0
             if count != len(self.population_score):
                 count += 1
 
-    #TODO: check success or not
+    # check success or not
     def check_success(self, chromosome):
         scores = []
         max_score = 0
@@ -41,8 +48,7 @@ class Population:
                     max_score = score
         return max_score
 
-    # TODO: calculate the score
-    #
+    # calculate the score
     def mashroom_hit(self, chromosome):
         score = 0
         for i in range(len(chromosome)-1):
@@ -69,18 +75,72 @@ class Population:
             score += self.kill_Gumpa(i)
             self.population_score[i]=score
 
-    # TODO: select based on scores
+    # select based on scores
     # select the best half of population
     def select(self):
         population_score = {k: v for k, v in sorted(self.population_score.items(), key=lambda item: item[1])}
-        print("population", population_score)
+        self.selectList = {k: population_score[k] for k in list(population_score)[100:]}
+        print("len", len(self.selectList))
+
+    def check_repeat(self, chromosome):
+        for i in range(len(chromosome)-1):
+            if (chromosome[i] == '1' and chromosome[i+1] == '1') or (chromosome[i] == '2' and chromosome[i+1] == '2') :
+                return False
+        return True
 
     def crossOver(self):
-        pass
+        population = dict()
+
+        count = 0
+        while count < 200:
+            parent1 = random.randint(0, 99)
+            parent2 = random.randint(0, 99)
+
+            while (True):
+                t = random.randint(1, len(self.level))
+
+                parent1str = list(self.selectList.items())[parent1][0]
+                parent2str = list(self.selectList.items())[parent2][0]
+
+                child1 = parent1str[:t] + parent2str[t:]
+                child2 = parent2str[:t] + parent1str[t:]
+
+                if self.check_repeat(child1) and self.check_repeat(child2):
+                    break
+
+            population[child1] = 0
+            if len(population) != count:
+                count += 1
+
+            population[child2] = 0
+            if len(population) != count:
+                count += 1
+
+            print("child , parent 1", child1, parent1str)
+            print("child , parent 2", child2, parent2str)
+
+        self.population_score = population
 
     def mutation(self):
-        pass
-chromoseme = Population('__G__M__')
-chromoseme.makePopulation()
-chromoseme.calculateScore()
-chromoseme.select()
+        population = dict()
+        for i in range(100):
+            t = random.randint(0, 10)
+            print("t in mu", t)
+            if t < 2:
+                if '1' in list(self.population_score.items())[i][0]:
+                    s = list(self.population_score.items())[i][0]
+                    s = s.replace('1', '0', 1)
+                    population[s] = 0
+            else:
+                population[list(self.population_score.items())[i][0]] = 0
+
+        self.population_score = population
+
+
+
+chromosome = Population('__G__M__')
+chromosome.makePopulation()
+chromosome.calculateScore()
+chromosome.select()
+chromosome.crossOver()
+chromosome.mutation()
